22155c4652487ecdfe621415b834b9966a786b6a
bradleypj@icloud.com <bradleypj@icloud.com>
2014-10-21 12:04:41 -0700

    add slides for week 3 day 2 and sample code

--- GithubToGo/GithubToGo/SplitContainerViewController.swift
+++ GithubToGo/GithubToGo/SplitContainerViewController.swift
@@ -15,6 +15,7 @@ class SplitContainerViewController: UIViewController, UISplitViewControllerDeleg
         
         let splitVC = self.childViewControllers[0] as UISplitViewController
         splitVC.delegate = self
+        //splitVC.viewControllers[0]
         // Do any additional setup after loading the view.
     }
 
@@ -24,6 +25,7 @@ class SplitContainerViewController: UIViewController, UISplitViewControllerDeleg
     }
     
     func splitViewController(splitViewController: UISplitViewController, collapseSecondaryViewController secondaryViewController: UIViewController!, ontoPrimaryViewController primaryViewController: UIViewController!) -> Bool {
+        println("collapsing")
         return true
     }
     
--- GithubToGo/GithubToGo/ViewController.swift
+++ GithubToGo/GithubToGo/ViewController.swift
@@ -9,9 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var firstLaunch = true
+    
+    var networkController : NetworkController!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
+        self.networkController = appDelegate.networkController
+        
+        self.networkController.requestOAuthAccess()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +28,5 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
