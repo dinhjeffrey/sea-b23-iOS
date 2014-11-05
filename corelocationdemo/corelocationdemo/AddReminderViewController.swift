00b605fa1700453f477e61e0b1432fe5f03af493
bradleypj@icloud.com <bradleypj@icloud.com>
2014-11-05 11:51:58 -0800

    added code and slides for week 5 day 3

--- corelocationdemo/corelocationdemo/AddReminderViewController.swift
+++ corelocationdemo/corelocationdemo/AddReminderViewController.swift
@@ -8,14 +8,19 @@
 
 import UIKit
 import MapKit
+import CoreData
 
 class AddReminderViewController: UIViewController {
     
     var locationManager : CLLocationManager!
     var selectedAnnotation : MKAnnotation!
+    var managedObjectContext : NSManagedObjectContext!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
+        self.managedObjectContext = appDelegate.managedObjectContext
         
 //        let regionSet = self.locationManager.monitoredRegions
 //        let regions = regionSet.allObjects
@@ -28,6 +33,17 @@ class AddReminderViewController: UIViewController {
         var geoRegion = CLCircularRegion(center: selectedAnnotation.coordinate, radius: 4000.0, identifier: "TestRegion")
         self.locationManager.startMonitoringForRegion(geoRegion)
         
+        //insert a reminder into the our DB
+        var newReminder = NSEntityDescription.insertNewObjectForEntityForName("Reminder", inManagedObjectContext: self.managedObjectContext) as Reminder
+        newReminder.name = "TestRegion"
+        
+        var error : NSError?
+        self.managedObjectContext.save(&error)
+        
+        //error checking
+        if error != nil {
+            println(error?.localizedDescription)
+        }
         NSNotificationCenter.defaultCenter().postNotificationName("REMINDER_ADDED", object: self, userInfo: ["region" : geoRegion])
         
         self.dismissViewControllerAnimated(true, completion: nil)
--- corelocationdemo/corelocationdemo/AppDelegate.swift
+++ corelocationdemo/corelocationdemo/AppDelegate.swift
@@ -21,17 +21,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillResignActive(application: UIApplication) {
+        println("will resign active")
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
 
     func applicationDidEnterBackground(application: UIApplication) {
+        println("application did enter background")
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
 
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        println("will enter foreground")
     }
 
     func applicationDidBecomeActive(application: UIApplication) {
@@ -65,7 +68,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent("corelocationdemo.sqlite")
         var error: NSError? = nil
         var failureReason = "There was an error creating or loading the application's saved data."
-        if coordinator!.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil, error: &error) == nil {
+        let options =
+        [   NSPersistentStoreUbiquitousContentNameKey: "CloudNotes", NSMigratePersistentStoresAutomaticallyOption: true, NSInferMappingModelAutomaticallyOption: true]
+        
+        if coordinator!.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: options, error: &error) == nil {
             coordinator = nil
             // Report any error we got.
             let dict = NSMutableDictionary()
